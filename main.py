5ec5ea5397ca5f1c4caf11237017fb111a70e332
Alexander <72577094+headrobot790@users.noreply.github.com>
2024-08-24 13:30:15 +0300

    Update main.py
    admin changed

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import asyncio
 from parcer import main, BETHOWEN, LINK
 
 if __name__ == "__main__":
+    print("a im admin and edit this changes")
     print("run program slowly")
     asyncio.run(main())
